e7e41454826b1e4b69b40c70a4a672cec1da1262
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-17 16:28:42 +0300

    adds components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import GETRequestBlock from "./components/GETRequestBlock";
+import POSTRequestBlock from "./components/POSTRequestBlock";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Hero />
+      <GETRequestBlock />
+      <POSTRequestBlock />
+    </>
+  );
 }
 
 export default App;
