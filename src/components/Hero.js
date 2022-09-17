1bbf77505cd7fb4b7d22785197dcbf9563095f4c
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-17 21:10:24 +0300

    adds content

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,14 +3,14 @@ import React from "react";
 export default function Hero() {
 	return (
 		<div className="hero">
-			<h1>Test assignment for front-end developer</h1>
+			<h1 className="hero__title">Test assignment for front-end developer</h1>
 
-			<p>
+			<p className="hero__subtitle">
 				What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with
 				accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.
 			</p>
 
-			<button className="button" type="button">
+			<button className="button hero__button" type="button">
 				Sing up
 			</button>
 		</div>
