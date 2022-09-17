e4756d01d655e174822161002f88273cd9f7d3b9
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-17 19:56:00 +0300

    adds logo and buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,23 @@
 import React from "react";
+import logo from "../assets/Logo.svg";
 
 export default function Heder() {
-	return <div>TEST</div>;
+	return (
+		<div className="header">
+			<div className="header__rectangle"></div>
+
+			<div className="header__main">
+				<img className="header__logo" src={logo} alt="Logo" />
+
+				<div className="header__buttons-block">
+					<button className="header__button" type="button">
+						Users
+					</button>
+					<button className="header__button" type="button">
+						Sing up
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
