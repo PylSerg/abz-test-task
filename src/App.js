11a4cc262285613e99743a36e7e66db27d4b1ee9
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-17 18:05:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Hero from "./components/Hero";
 import GETRequestBlock from "./components/GETRequestBlock";
 import POSTRequestBlock from "./components/POSTRequestBlock";
 
+import "./sass/style.scss";
+
 function App() {
 	return (
 		<>
