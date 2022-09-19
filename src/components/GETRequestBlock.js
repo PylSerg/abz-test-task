679fae073733e12a809e63352f49ecf0a39ce3fd
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-19 20:40:34 +0300

    adds content

--- src/components/GETRequestBlock.js
+++ src/components/GETRequestBlock.js
@@ -6,13 +6,29 @@ export default function GETRequestBlock() {
 		<div className="get-block">
 			<h1>Working with GET request</h1>
 
-			<ul className="get-block__users">
+			<ul className="get-block__users-list">
 				<li className="get-block__user">
-					<img src={photoCover} alt="User avatar" />
-					<p>User name</p>
-					<p>User position</p>
-					<p>User email</p>
-					<p>User phone number</p>
+					<img className="get-block-user-avatar" src={photoCover} alt="User avatar" />
+					<p className="get-block__user-name">User name is Jhon Spilberg and eschejhkjhkjhjk kto-to podobnyi</p>
+					<p className="get-block__user-position">User position kjlfdjfk lgjljg kfldjglkfdjgk jdflgjlfdjgldfjg jf</p>
+					<a href="mailto:fjkhgffj@ffgfg.fkjgfl" className="get-block__user-email" title="User email">
+						User email dfh khhkjghfkjghkjfhgkjhfdgkjhfdkjhgfdkhgkjh
+					</a>
+					<a href="tel:+380682383696" className="get-block__user-phone">
+						User phone number
+					</a>
+				</li>
+
+				<li className="get-block__user">
+					<img className="get-block-user-avatar" src={photoCover} alt="User avatar" />
+					<p className="get-block__user-name">User name</p>
+					<p className="get-block__user-position">User position</p>
+					<a href="mailto:fjkhgffj@ffgfg.fkjgfl" className="get-block__user-email" title="User email">
+						User email
+					</a>
+					<a href="tel:+380682383696" className="get-block__user-phone">
+						User phone number
+					</a>
 				</li>
 			</ul>
 		</div>
