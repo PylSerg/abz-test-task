608e7b4eec3b9d6c08f4560e4b606d2f89af527c
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-19 23:02:06 +0300

    adds content

--- src/components/POSTRequestBlock.js
+++ src/components/POSTRequestBlock.js
@@ -1,5 +1,36 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function POSTRequestBlock() {
-	return <div></div>;
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm();
+
+	return (
+		<div className="post-block">
+			<h1>Working with POST request</h1>
+
+			<form className="user-form">
+				<label className="user-form__label">
+					<input className="user-form__field" type="text" placeholder=" " {...register} required />
+					<span className="user-form__title">Your name</span>
+				</label>
+
+				<label className="user-form__label">
+					<input className="user-form__field" type="email" placeholder=" " required />
+					<span className="user-form__title">Email</span>
+				</label>
+
+				<label className="user-form__label">
+					<input className="user-form__field" type="tel" pattern="[+][3][8][0-9]{10}" placeholder="+38" required />
+					<span className="user-form__title">Phone</span>
+				</label>
+
+				<input type="submit" value="Sing up" />
+			</form>
+		</div>
+	);
 }
