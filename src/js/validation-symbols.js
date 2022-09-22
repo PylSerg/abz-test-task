f5b6cd14de5a4afa876a63e6a144daff51c90ea6
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-22 13:23:56 +0300

    adds symbols for validation

---
+++ src/js/validation-symbols.js
@@ -0,0 +1,5 @@
+export const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export const symbolsArray = ["+", "=", ".", "~", "!", "?", "#", "$", "%", "^", "&", "_", "{", "}"];
+
+export const specialSymbolsArray = ["/", "|", "*", ",", "(", ")", "[", "]", ":", ";", "@", "<", ">", " "];
