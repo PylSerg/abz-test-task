76bdbdd28dd8495f1f347e5c06fc06fb76964651
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-23 21:08:21 +0300

    adds position section

--- src/components/POSTRequestBlock.js
+++ src/components/POSTRequestBlock.js
@@ -1,4 +1,4 @@
-import { useState, createRef } from "react";
+import { useState, useEffect, createRef } from "react";
 import { useForm } from "react-hook-form";
 import { generate as genID } from "shortid";
 import { numbersArray, symbolsArray, specialSymbolsArray } from "../js/validation-symbols";
@@ -6,13 +6,20 @@ import { numbersArray, symbolsArray, specialSymbolsArray } from "../js/validatio
 export default function POSTRequestBlock() {
 	const [validity, setValidity] = useState({ name: true, email: true, phone: true });
 	const [fieldReadyToSend, setFieldReadyToSend] = useState({ name: false, email: false, phone: false });
-	const { register, handleSubmit, watch, reset } = useForm();
+	const [checked, setChecked] = useState(true);
+	const { register, handleSubmit, watch } = useForm();
 
 	const nameError = createRef();
 	const emailError = createRef();
 	const phoneError = createRef();
 
 	const send = fieldReadyToSend.name && fieldReadyToSend.email && fieldReadyToSend.phone;
+
+	console.log(`\x1b[33m ${watch("position")}`);
+
+	useEffect(() => {
+		setChecked(null);
+	}, []);
 
 	// Dynamic styles
 	function inputStyle(el) {
@@ -28,7 +35,7 @@ export default function POSTRequestBlock() {
 	}
 
 	function buttonStyle() {
-		return send ? "button" : "button button--disabled";
+		return send ? "button user-form__button" : "button user-form__button button--disabled";
 	}
 
 	function disabledButton() {
@@ -187,10 +194,6 @@ export default function POSTRequestBlock() {
 
 		console.log(req);
 
-		reset();
-
-		setFieldReadyToSend({ name: false, email: false, phone: false });
-
 		return console.log(`\x1b[32m Request status: OK`);
 	}
 
@@ -200,23 +203,61 @@ export default function POSTRequestBlock() {
 			<h1>Working with POST request</h1>
 
 			<form className="user-form" onSubmit={handleSubmit(onSubmit)}>
-				<label className="user-form__label">
-					<input className={inputStyle("name")} autoComplete="none" placeholder=" " {...register("name")} onBlur={nameValidator} />
-					<span className={titleStyle("name")}>Your name</span>
-					<span className={errorStyle("name")} ref={nameError} />
-				</label>
+				{/* Input block */}
+				<div>
+					<label className="user-form__label">
+						<input className={inputStyle("name")} autoComplete="none" placeholder=" " {...register("name")} onBlur={nameValidator} />
+						<span className={titleStyle("name")}>Your name</span>
+						<span className={errorStyle("name")} ref={nameError} />
+					</label>
 
-				<label className="user-form__label">
-					<input className={inputStyle("email")} autoComplete="none" placeholder=" " {...register("email")} onBlur={emailValidator} />
-					<span className={titleStyle("email")}>Email</span>
-					<span className={errorStyle("email")} ref={emailError} />
-				</label>
+					<label className="user-form__label">
+						<input className={inputStyle("email")} autoComplete="none" placeholder=" " {...register("email")} onBlur={emailValidator} />
+						<span className={titleStyle("email")}>Email</span>
+						<span className={errorStyle("email")} ref={emailError} />
+					</label>
 
-				<label className="user-form__label">
-					<input className={inputStyle("phone")} autoComplete="none" placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
-					<span className={titleStyle("phone")}>Phone</span>
-					<span className={errorStyle("phone")} ref={phoneError} />
-				</label>
+					<label className="user-form__label">
+						<input className={inputStyle("phone")} autoComplete="none" placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
+						<span className={titleStyle("phone")}>Phone</span>
+						<span className={errorStyle("phone")} ref={phoneError} />
+					</label>
+				</div>
+
+				{/* Position block */}
+				<div className="user-form__position-block">
+					<p>Select your position</p>
+
+					<div className="user-form__radio-block">
+						<div className="user-form__radio-position">
+							<input type="radio" id="frontendDeveloper" value="Frontend developer" {...register("position")} checked={checked} />
+							<label htmlFor="frontendDeveloper" className="user-form__radio-label">
+								Frontend developer
+							</label>
+						</div>
+
+						<div className="user-form__radio-position">
+							<input type="radio" id="backendDeveloper" value="Backend developer" {...register("position")} />
+							<label htmlFor="backendDeveloper" className="user-form__radio-label">
+								Backend developer
+							</label>
+						</div>
+
+						<div className="user-form__radio-position">
+							<input type="radio" id="designer" value="Designer" {...register("position")} />
+							<label htmlFor="designer" className="user-form__radio-label">
+								Designer
+							</label>
+						</div>
+
+						<div className="user-form__radio-position">
+							<input type="radio" id="qa" value="QA" {...register("position")} />
+							<label htmlFor="qa" className="user-form__radio-label">
+								QA
+							</label>
+						</div>
+					</div>
+				</div>
 
 				<input className={buttonStyle()} type="submit" value="Sing up" disabled={disabledButton()} />
 			</form>
