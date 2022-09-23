0866f99395186931485b19f1e30c2a0e67979210
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-23 13:42:53 +0300

    adds rules

--- src/components/POSTRequestBlock.js
+++ src/components/POSTRequestBlock.js
@@ -5,13 +5,16 @@ import { numbersArray, symbolsArray, specialSymbolsArray } from "../js/validatio
 
 export default function POSTRequestBlock() {
 	const [validity, setValidity] = useState({ name: true, email: true, phone: true });
+	const [fieldReadyToSend, setFieldReadyToSend] = useState({ name: false, email: false, phone: false });
 	const { register, handleSubmit, watch, reset } = useForm();
 
 	const nameError = createRef();
 	const emailError = createRef();
 	const phoneError = createRef();
 
-	// Styles
+	const send = fieldReadyToSend.name && fieldReadyToSend.email && fieldReadyToSend.phone;
+
+	// Dynamic styles
 	function inputStyle(el) {
 		return validity[el] ? "user-form__field" : "user-form__field user-form__field-error";
 	}
@@ -24,13 +27,23 @@ export default function POSTRequestBlock() {
 		return validity[el] ? "user-form__error user-form__error--hidden" : "user-form__error";
 	}
 
+	function buttonStyle() {
+		return send ? "button" : "button button--disabled";
+	}
+
+	function disabledButton() {
+		if (send) return false;
+
+		return true;
+	}
+
 	// Name validation
 	function nameValidator() {
 		const arrayExceptionForName = numbersArray + symbolsArray + specialSymbolsArray;
 
 		if (!watch("name")) {
 			nameError.current.textContent = `Field can't be empty!`;
-			setValidity({ ...validity, name: false });
+			makeNameInvalid();
 
 			return false;
 		}
@@ -39,13 +52,19 @@ export default function POSTRequestBlock() {
 			if (watch("name").includes(arrayExceptionForName[i])) {
 				nameError.current.textContent = `Name can contain only letters or "-"`;
 
-				setValidity({ ...validity, name: false });
+				makeNameInvalid();
 
 				return false;
 			}
 		}
 
+		function makeNameInvalid() {
+			setValidity({ ...validity, name: false });
+			setFieldReadyToSend({ ...fieldReadyToSend, name: false });
+		}
+
 		setValidity({ ...validity, name: true });
+		setFieldReadyToSend({ ...fieldReadyToSend, name: true });
 
 		return true;
 	}
@@ -56,28 +75,32 @@ export default function POSTRequestBlock() {
 
 		if (!watch("email")) {
 			emailError.current.textContent = `Field can't be empty!`;
-			setValidity({ ...validity, email: false });
+
+			makeEmailInvalid();
 
 			return false;
 		}
 
 		if (!watch("email").includes("@")) {
 			emailError.current.textContent = `Email must contain "@"`;
-			setValidity({ ...validity, email: false });
+
+			makeEmailInvalid();
 
 			return false;
 		}
 
 		if (!emailParts[0] || !emailParts[1]) {
 			emailError.current.textContent = `Invalid email format!`;
-			setValidity({ ...validity, email: false });
+
+			makeEmailInvalid();
 
 			return false;
 		}
 
 		if (!emailParts[1].includes(".")) {
 			emailError.current.textContent = `Invalid domain format!`;
-			setValidity({ ...validity, email: false });
+
+			makeEmailInvalid();
 
 			return false;
 		}
@@ -86,13 +109,19 @@ export default function POSTRequestBlock() {
 			if (emailParts[0].includes(specialSymbolsArray[i])) {
 				emailError.current.textContent = `Email can't contain special symbols!`;
 
-				setValidity({ ...validity, email: false });
+				makeEmailInvalid();
 
 				return false;
 			}
 		}
 
+		function makeEmailInvalid() {
+			setValidity({ ...validity, email: false });
+			setFieldReadyToSend({ ...fieldReadyToSend, email: false });
+		}
+
 		setValidity({ ...validity, email: true });
+		setFieldReadyToSend({ ...fieldReadyToSend, email: true });
 
 		return true;
 	}
@@ -104,33 +133,43 @@ export default function POSTRequestBlock() {
 
 		if (!watch("phone")) {
 			phoneError.current.textContent = `Field can't be empty!`;
-			setValidity({ ...validity, phone: false });
+
+			makePhoneInvalid();
 
 			return false;
 		}
 
 		if (!watch("phone").includes("+")) {
 			phoneError.current.textContent = `Phone format must be +380XXXXXXXXX`;
-			setValidity({ ...validity, phone: false });
+
+			makePhoneInvalid();
 
 			return false;
 		}
 
 		if (!phoneValid) {
 			phoneError.current.textContent = `Phone number can contain only numbers and "+"`;
-			setValidity({ ...validity, phone: false });
+
+			makePhoneInvalid();
 
 			return false;
 		}
 
 		if (phoneFormat.length !== 13) {
 			phoneError.current.textContent = `Invalid phone number!`;
-			setValidity({ ...validity, phone: false });
+
+			makePhoneInvalid();
 
 			return false;
 		}
 
+		function makePhoneInvalid() {
+			setValidity({ ...validity, phone: false });
+			setFieldReadyToSend({ ...fieldReadyToSend, phone: false });
+		}
+
 		setValidity({ ...validity, phone: true });
+		setFieldReadyToSend({ ...fieldReadyToSend, phone: true });
 
 		return true;
 	}
@@ -160,24 +199,24 @@ export default function POSTRequestBlock() {
 
 			<form className="user-form" onSubmit={handleSubmit(onSubmit)}>
 				<label className="user-form__label">
-					<input className={inputStyle("name")} placeholder=" " {...register("name")} onBlur={nameValidator} />
+					<input className={inputStyle("name")} autoComplete="none" placeholder=" " {...register("name")} onBlur={nameValidator} />
 					<span className={titleStyle("name")}>Your name</span>
 					<span className={errorStyle("name")} ref={nameError} />
 				</label>
 
 				<label className="user-form__label">
-					<input className={inputStyle("email")} placeholder=" " {...register("email")} onBlur={emailValidator} />
+					<input className={inputStyle("email")} autoComplete="none" placeholder=" " {...register("email")} onBlur={emailValidator} />
 					<span className={titleStyle("email")}>Email</span>
 					<span className={errorStyle("email")} ref={emailError} />
 				</label>
 
 				<label className="user-form__label">
-					<input className={inputStyle("phone")} placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
+					<input className={inputStyle("phone")} autoComplete="none" placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
 					<span className={titleStyle("phone")}>Phone</span>
 					<span className={errorStyle("phone")} ref={phoneError} />
 				</label>
 
-				<input className="button" type="submit" value="Sing up" />
+				<input className={buttonStyle()} type="submit" value="Sing up" disabled={disabledButton()} />
 			</form>
 		</div>
 	);
