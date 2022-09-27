3c270cda9e56c29e5baff802c2fbcdecf7eb0d69
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-27 15:42:10 +0300

    adds successful registration message

--- src/components/POSTRequestBlock.js
+++ src/components/POSTRequestBlock.js
@@ -2,8 +2,10 @@ import { useState, useEffect, createRef } from "react";
 import { useForm } from "react-hook-form";
 import { generate as genID } from "shortid";
 import { numbersArray, symbolsArray, specialSymbolsArray } from "../js/validation-symbols";
+import successImage from "../assets/success-image.svg";
 
 export default function POSTRequestBlock() {
+	const [newUser, setNewUser] = useState(true);
 	const [validity, setValidity] = useState({ name: true, email: true, phone: true, photo: true });
 	const [possibilityOfSending, setPossibilityOfSending] = useState({ name: false, email: false, phone: false, photo: true });
 	const [checked, setChecked] = useState(true);
@@ -263,6 +265,8 @@ export default function POSTRequestBlock() {
 
 		console.log(req);
 
+		setNewUser(false);
+
 		return console.log(`\x1b[32m Request status: OK`);
 	}
 
@@ -270,87 +274,96 @@ export default function POSTRequestBlock() {
 		<div className="post-block">
 			<h1>Working with POST request</h1>
 
-			<form className="user-form" encType="multipart/form-data" action="/upload/image" onSubmit={handleSubmit(onSubmit)}>
-				{/*
+			{newUser && (
+				<form className="user-form" encType="multipart/form-data" action="/upload/image" onSubmit={handleSubmit(onSubmit)}>
+					{/*
 					Contact block
 				*/}
-				<div>
-					<label className="user-form__label">
-						<input className={inputStyle("name")} autoComplete="none" placeholder=" " {...register("name")} onBlur={nameValidator} />
-						<span className={titleStyle("name")}>Your name</span>
-						<span className={errorStyle("name")} ref={nameError} />
-					</label>
+					<div>
+						<label className="user-form__label">
+							<input className={inputStyle("name")} autoComplete="none" placeholder=" " {...register("name")} onBlur={nameValidator} />
+							<span className={titleStyle("name")}>Your name</span>
+							<span className={errorStyle("name")} ref={nameError} />
+						</label>
 
-					<label className="user-form__label">
-						<input className={inputStyle("email")} autoComplete="none" placeholder=" " {...register("email")} onBlur={emailValidator} />
-						<span className={titleStyle("email")}>Email</span>
-						<span className={errorStyle("email")} ref={emailError} />
-					</label>
+						<label className="user-form__label">
+							<input className={inputStyle("email")} autoComplete="none" placeholder=" " {...register("email")} onBlur={emailValidator} />
+							<span className={titleStyle("email")}>Email</span>
+							<span className={errorStyle("email")} ref={emailError} />
+						</label>
 
-					<label className="user-form__label">
-						<input className={inputStyle("phone")} autoComplete="none" placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
-						<span className={titleStyle("phone")}>Phone</span>
-						<span className={errorStyle("phone")} ref={phoneError} />
-					</label>
-				</div>
+						<label className="user-form__label">
+							<input className={inputStyle("phone")} autoComplete="none" placeholder="+38" {...register("phone")} onBlur={phoneValidator} />
+							<span className={titleStyle("phone")}>Phone</span>
+							<span className={errorStyle("phone")} ref={phoneError} />
+						</label>
+					</div>
 
-				{/*
+					{/*
 					Position block
 				*/}
-				<div className="user-form__position-block">
-					<p>Select your position</p>
+					<div className="user-form__position-block">
+						<p>Select your position</p>
 
-					<div className="user-form__radio-block">
-						<div className="user-form__radio-position">
-							<input type="radio" id="frontendDeveloper" value="Frontend developer" {...register("position")} checked={checked} />
-							<label htmlFor="frontendDeveloper" className="user-form__radio-label">
-								Frontend developer
-							</label>
-						</div>
+						<div className="user-form__radio-block">
+							<div className="user-form__radio-position">
+								<input type="radio" id="frontendDeveloper" value="Frontend developer" {...register("position")} checked={checked} />
+								<label htmlFor="frontendDeveloper" className="user-form__radio-label">
+									Frontend developer
+								</label>
+							</div>
 
-						<div className="user-form__radio-position">
-							<input type="radio" id="backendDeveloper" value="Backend developer" {...register("position")} />
-							<label htmlFor="backendDeveloper" className="user-form__radio-label">
-								Backend developer
-							</label>
-						</div>
+							<div className="user-form__radio-position">
+								<input type="radio" id="backendDeveloper" value="Backend developer" {...register("position")} />
+								<label htmlFor="backendDeveloper" className="user-form__radio-label">
+									Backend developer
+								</label>
+							</div>
 
-						<div className="user-form__radio-position">
-							<input type="radio" id="designer" value="Designer" {...register("position")} />
-							<label htmlFor="designer" className="user-form__radio-label">
-								Designer
-							</label>
-						</div>
+							<div className="user-form__radio-position">
+								<input type="radio" id="designer" value="Designer" {...register("position")} />
+								<label htmlFor="designer" className="user-form__radio-label">
+									Designer
+								</label>
+							</div>
 
-						<div className="user-form__radio-position">
-							<input type="radio" id="qa" value="QA" {...register("position")} />
-							<label htmlFor="qa" className="user-form__radio-label">
-								QA
-							</label>
+							<div className="user-form__radio-position">
+								<input type="radio" id="qa" value="QA" {...register("position")} />
+								<label htmlFor="qa" className="user-form__radio-label">
+									QA
+								</label>
+							</div>
 						</div>
 					</div>
-				</div>
 
-				{/*
+					{/*
 					Upload image
 				*/}
-				<div className="user-form__photo-block">
-					<input className="user-form__upload-photo" type="file" id="userPhoto" placeholder="Upload your photo" {...register("photo")} onBlur={photoFieldController} />
+					<div className="user-form__photo-block">
+						<input className="user-form__upload-photo" type="file" id="userPhoto" placeholder="Upload your photo" {...register("photo")} onBlur={photoFieldController} />
 
-					<label className={uploadPhotoBUtton()} htmlFor="userPhoto">
-						Upload
-					</label>
+						<label className={uploadPhotoBUtton()} htmlFor="userPhoto">
+							Upload
+						</label>
 
-					<div className={uploadFieldStyle()}>{photoName}</div>
+						<div className={uploadFieldStyle()}>{photoName}</div>
 
-					<span className={errorStyle("photo")} ref={photoError} />
-				</div>
+						<span className={errorStyle("photo")} ref={photoError} />
+					</div>
 
-				{/*
+					{/*
 					Submit form
 				*/}
-				<input className={sendButtonStyle()} type="submit" value="Sing up" disabled={disabledSendButton()} />
-			</form>
+					<input className={sendButtonStyle()} type="submit" value="Sing up" disabled={disabledSendButton()} />
+				</form>
+			)}
+
+			{!newUser && (
+				<div className="user-registered">
+					<h1>User successfully registered</h1>
+					<img className="user-registered__image" src={successImage} alt="Successfully registered" />
+				</div>
+			)}
 		</div>
 	);
 }
