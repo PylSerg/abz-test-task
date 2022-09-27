b76f07e36949b6d8513a4f7baedf3d01cbfcf742
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-27 21:02:58 +0300

    adds get request

--- src/components/GETRequestBlock.js
+++ src/components/GETRequestBlock.js
@@ -1,35 +1,43 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import photoCover from "../assets/photo-cover.svg";
 
 export default function GETRequestBlock() {
+	const [users, setUsers] = useState([]);
+	console.log(users);
+
+	useEffect(() => {
+		getUsersList();
+	}, []);
+
+	async function getUsersList() {
+		const response = await axios.get(`https://frontend-test-assignment-api.abz.agency/api/v1/users?page=3&count=6`);
+
+		const newUsersList = [...users].sort((a, b) => b.registration_timestamp - a.registration_timestamp);
+
+		response.data.users.map(user => newUsersList.push(user));
+
+		setUsers(newUsersList);
+	}
+
 	return (
 		<div className="get-block">
 			<h1>Working with GET request</h1>
 
 			<ul className="get-block__users-list">
-				<li className="get-block__user">
-					<img className="get-block-user-avatar" src={photoCover} alt="User avatar" />
-					<p className="get-block__user-name">User name is Jhon Spilberg and eschejhkjhkjhjk kto-to podobnyi</p>
-					<p className="get-block__user-position">User position kjlfdjfk lgjljg kfldjglkfdjgk jdflgjlfdjgldfjg jf</p>
-					<a className="get-block__user-email" href="mailto:fjkhgffj@ffgfg.fkjgfl" title="User email">
-						User email dfh khhkjghfkjghkjfhgkjhfdgkjhfdkjhgfdkhgkjh
-					</a>
-					<a className="get-block__user-phone" href="tel:+380682383696">
-						User phone number
-					</a>
-				</li>
-
-				<li className="get-block__user">
-					<img className="get-block-user-avatar" src={photoCover} alt="User avatar" />
-					<p className="get-block__user-name">User name</p>
-					<p className="get-block__user-position">User position</p>
-					<a href="mailto:fjkhgffj@ffgfg.fkjgfl" className="get-block__user-email" title="User email">
-						User email
-					</a>
-					<a href="tel:+380682383696" className="get-block__user-phone">
-						User phone number
-					</a>
-				</li>
+				{users.map(user => (
+					<li className="get-block__user" key={user.id}>
+						<img className="get-block-user-avatar" src={user?.photo} alt={user?.name} />
+						<p className="get-block__user-name">{user?.name}</p>
+						<p className="get-block__user-position">{user?.position}</p>
+						<a className="get-block__user-email" href={`mailto:${user?.email}`} title={user?.email}>
+							{user?.email}
+						</a>
+						<a className="get-block__user-phone" href={`tel:${user?.phone}`}>
+							{user?.phone}
+						</a>
+					</li>
+				))}
 			</ul>
 
 			<button className="button get-block__button" type="button">
