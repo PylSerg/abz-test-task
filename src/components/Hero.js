14868e88b3fa42d544b364d0a4322e58bf05e7af
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-09-29 22:32:20 +0300

    adds DOM element

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,12 +4,14 @@ import { goToForm } from "../js/anchor-crossing";
 export default function Hero() {
 	return (
 		<div className="hero">
-			<h1 className="hero__title">Test assignment for front-end developer</h1>
+			<div className="hero__text">
+				<h1 className="hero__title">Test assignment for front-end developer</h1>
 
-			<p className="hero__subtitle">
-				What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with
-				accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.
-			</p>
+				<p className="hero__subtitle">
+					What defines a good front-end developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces
+					with accessibility in mind. They should also be excited to learn, as the world of Front-End Development keeps evolving.
+				</p>
+			</div>
 
 			<button className="button hero__button" type="button" onClick={goToForm}>
 				Sing up
